a697182da9c4e4536553847f0d8cf928fff2eeb1
katya-sav <esava1105@gmail.com>
2024-03-29 09:46:03 +0300

    done lection3 homework

--- lection_3/homework.js
+++ lection_3/homework.js
@@ -1,4 +1,3 @@
-
 // //1
 // const user = {
 //     name: 'Bob',
@@ -23,21 +22,20 @@
 //         }
 //     },
 // };
-//
-// user.funcFunc()(); // ?
-// user.funcArrow()(); // ?
-// user.arrowFunc()(); // ?
-// user.arrowArrow()(); // ?
+
+// user.funcFunc()(); // undefined
+// user.funcArrow()(); // { user }
+// user.arrowFunc()(); // undefined
+// user.arrowArrow()(); // undefined
 
 // 2
 // var poke1 = {name:'Pikachu'};
 // var poke2 = {name:'Chermander'};
 // var poke3 = {name:'Bulbasaur'};
-//
-// var sayName = function(){ console.log(this.name) }
-//
-// sayName.bind(poke1).bind(poke2).call(poke3);
 
+// var sayName = function(){ console.log(this.name) }
+
+// sayName.bind(poke1).bind(poke2).call(poke3); // "Pikachu"
 
 // 3
 // const obj = {
@@ -53,55 +51,55 @@
 //     }
 // }
 //
-// obj.showFirstName(); // ?
-// obj.showLastName(); // ?=
+// obj.showFirstName(); // 'Bill'
+// obj.showLastName(); // undefined
 //
-// obj.showFirstName.bind({ firstName: 'Boris' })(); // ?
-// obj.showFirstName.bind({ firstName: 'Boris' }).bind({ firstName: 'Oleg' })(); // ?
+// obj.showFirstName.bind({ firstName: 'Boris' })(); // "Boris"
+// obj.showFirstName.bind({ firstName: 'Boris' }).bind({ firstName: 'Oleg' })(); // "Boris"
 //
-// obj.showLastName.bind({ lastName: 'Boris' })(); // ?
+// obj.showLastName.bind({ lastName: 'Boris' })(); // undefined
 
 // 4
 
 const user = {
-    name: 'Mike',
-    fn: function () {
-        console.log(this.name)
-    }
-}
+  name: "Mike",
+  fn: function () {
+    console.log(this.name);
+  },
+};
 
-setTimeout(user.fn, 1000)
-
-
+setTimeout(user.fn, 1000);
 
 // Что будет выведено в консоль после истечения таймаута и почему?
+// Ответ: udefined, тк произошла потеря контекста
 // Сделайте так, чтоб починить и выводило "Mike"
+
+let func = user.fn.bind(user);
+
+func();
+setTimeout(func, 1000);
 
 // Подсказка - ответ найдете в 5 ссылке README
 
 // 5
 //Исправьте cтроку(***), чтобы всё работало (других строк изменять не надо, кроме password, чтобы проверить if else).
 //
-// function askPassword(ok, fail) {
-//   let password = 'rockstar2'
-//   if (password == "rockstar") ok();
-//   else fail();
-// }
-//
-// let user = {
-//   name: 'Вася',
-//
-//   loginOk() {
-//     console.log(`${this.name} logged in`);
-//   },
-//
-//   loginFail() {
-//     console.log(`${this.name} failed to log in`);
-//   },
-//
-// };
-//
-// askPassword(user.loginOk, user.loginFail) //***;
+function askPassword(ok, fail) {
+  let password = "rockstar2";
+  if (password == "rockstar") ok();
+  else fail();
+}
 
+let user1 = {
+  name: "Вася",
 
+  loginOk() {
+    console.log(`${this.name} logged in`);
+  },
 
+  loginFail() {
+    console.log(`${this.name} failed to log in`);
+  },
+};
+
+askPassword(user1.loginOk.bind(user1), user1.loginFail.bind(user1));
